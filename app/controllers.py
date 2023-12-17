4f0c0a1c5ac6addf0d8bfdc2750d000af0371f2e
Gilmar Campoverde <gcampoverdea@est.ups.edu.ec>
2023-12-17 12:43:14 -0500

    Def MC

---
+++ app/controllers.py
@@ -0,0 +1,17 @@
+from typing import List
+from datetime import datetime
+from app.models import Cliente, Cuenta_Ahorro, Transaccion
+
+fake_db = []
+
+def create_cliente(cliente: Cliente):
+    # Lógica para crear un cliente
+    return {"mensaje": "Cliente creado exitosamente", "cliente": cliente}
+
+def create_cuenta(cuenta: Cuenta_Ahorro):
+    # Lógica para crear una cuenta de ahorro
+    return {"mensaje": "Cuenta creada exitosamente", "cuenta": cuenta}
+
+def create_transaccion(transaccion: Transaccion):
+    # Lógica para crear una transacción
+    return {"mensaje": "Transaccion exitosa", "transaccion": transaccion}
--- main.py
+++ main.py
@@ -1,54 +1,21 @@
-"""
-Integrantes:
-José Camacho
-Gilmar Campoverde
-Josue Cueva
-Santiago Miño
-"""
-from typing import Union
+# backendSistemaAhorros/main.py
 from fastapi import FastAPI
-from pydantic import BaseModel
-from datetime import datetime
-
+from app import create_cliente, create_cuenta, create_transaccion
 
 app = FastAPI()
 
-#classes
-class Cliente(BaseModel):
-    id_cliente: int
-    nombre:str
-    correo_electronico:str
-    password:str
-
-class Cuenta_Ahorro(BaseModel):
-    id_cuenta: int
-    saldo: float
-    id_usuario: int
-
-class Transaccion(BaseModel):
-    id_transaccion:int
-    id_cuenta:int
-    tipo_transaccion:int
-    monto:float
-    fecha_transaccion: datetime  
-
-fake_db = []
-
-#controllers
-
-#Routes
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.post("/clientes/")
-async def create_cliente(cliente: Cliente):
-    return {"mensaje": "Cliente creado exitosamente", "cliente": cliente}
+async def post_cliente(cliente: Cliente):
+    return create_cliente(cliente)
 
 @app.post("/cuentas/")
-async def create_cuenta(cuenta: Cuenta_Ahorro):
-    return {"mensaje": "Cuenta creada exitosamente", "cuenta": cuenta}
+async def post_cuenta(cuenta: Cuenta_Ahorro):
+    return create_cuenta(cuenta)
 
 @app.post("/transacciones/")
-async def create_transaccion(transaccion: Transaccion):
-    return {"mensaje": "Transaccion exitosa", "transaccion": transaccion}
+async def post_transaccion(transaccion: Transaccion):
+    return create_transaccion(transaccion)

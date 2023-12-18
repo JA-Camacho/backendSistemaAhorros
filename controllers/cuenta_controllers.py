890699b0ec2869171dc2f1883b7134fd4b83365d
Gilmar Campoverde <gcampoverdea@est.ups.edu.ec>
2023-12-18 01:09:37 -0500

    add GetCuentas

--- controllers/cuenta_controllers.py
+++ controllers/cuenta_controllers.py
@@ -1,5 +1,7 @@
 from models.cuenta_ahorro import Cuenta_Ahorro
 from sqlalchemy import text
+from fastapi import HTTPException
+from controllers.cliente_controllers import valor_md5
 from database.database import engine
 
 def create_cuenta(cuenta: Cuenta_Ahorro):
@@ -12,3 +14,16 @@ def delete_cuenta(cuenta: Cuenta_Ahorro):
     with engine.connect() as connection:
         query = text("DELETE FROM cuentas_ahorro WHERE id_cuenta = :id_cuenta")
         connection.execute(query, id_cuenta=cuenta.id_cuenta)
+
+def get_cuentas_usuario(correo: str, contrasena: str):
+    try:
+        with engine.connect() as connection:
+            query = text("SELECT id_cuenta, id_usuario, saldo FROM cuentas_ahorro WHERE id_usuario = ("
+                         "SELECT id_cliente FROM clientes WHERE correo_electronico = :correo_electronico AND password = :contra_md5)"
+                         )
+            result = connection.execute(query, correo_electronico=correo, contra_md5=valor_md5(contrasena)).fetchall()
+
+            cuentas_data = [dict(row) for row in result]
+            return cuentas_data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al obtener cuentas del usuario: {str(e)}")
--- views/cuenta_views.py
+++ views/cuenta_views.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from models.cuenta_ahorro import Cuenta_Ahorro
-from controllers.cuenta_controllers import create_cuenta, delete_cuenta
+from controllers.cuenta_controllers import create_cuenta, delete_cuenta, get_cuentas_usuario
 
 router = APIRouter()
 
@@ -14,3 +14,14 @@ async def create_cuenta_endpoint(cuenta: Cuenta_Ahorro):
 async def delete_cuenta_endpoint(cuenta: Cuenta_Ahorro):
     result = delete_cuenta(cuenta)
     return {"mensaje": "Cliente eliminado correctamente"}
+
+@router.get("/cuentas/")
+async def get_cuentas_usuario_endpoint(
+    correo: str = Query(..., description="Correo electrónico del usuario"),
+    contrasena: str = Query(..., description="Contraseña del usuario")
+):
+    try:
+        cuentas = get_cuentas_usuario(correo, contrasena)
+        return {"mensaje": "Cuentas obtenidas correctamente", "cuentas": cuentas}
+    except HTTPException as e:
+        return {"mensaje": "Error al procesar la solicitud", "detalle": e.detail}

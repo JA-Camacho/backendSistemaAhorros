bc14d3d081a7b09040c9a5049ac66360ee2963dc
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-12-17 21:46:53 -0500

    Create CRUDs: create and delete clientes

--- controllers/cliente_controllers.py
+++ controllers/cliente_controllers.py
@@ -1,11 +1,13 @@
-from typing import List
-from models import Cliente
-from database import insert_cliente
+from models.cliente import Cliente
+from database.database import engine
 
 def create_cliente(cliente: Cliente):
+    with engine.connect() as connection:
+        connection.execute('insert into clientes values ({0},\'{1}\',\'{2}\',\'{3}\')'.
+                           format(cliente.id_cliente,cliente.nombre,cliente.correo_electronico,cliente.password,cliente))
     return {"mensaje": "Cliente creado exitosamente", "cliente": cliente}
 
-
-async def create_cliente(cliente: Cliente):
-    await insert_cliente(cliente.nombre, cliente.correo_electronico, cliente.password)
-    return {"mensaje": "Cliente creado exitosamente", "cliente": cliente.dict()}
+def delete_cliente(cliente: Cliente):
+    with engine.connect() as connection:
+        connection.execute('delete from clientes where id_cliente = {0}'.
+                           format(cliente.id_cliente))
--- views/cliente_views.py
+++ views/cliente_views.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException
-from controllers import create_cliente
 from models.cliente import Cliente
+from controllers.cliente_controllers import create_cliente, delete_cliente
 
 router = APIRouter()
 
-@router.post("/clientes/", response_model=dict)
-async def create_client_route(cliente: Cliente):
-    return await create_cliente(cliente)
+@router.post("/clientes/")
+async def create_cliente_endpoint(cliente: Cliente):
+    result = create_cliente(cliente)
+    return {"mensaje": "Cliente creado exitosamente", "cliente": result["cliente"]}
 
+
+@router.delete("/clientes/")
+async def delete_cliente_endpoint(cliente: Cliente):
+    result = delete_cliente(cliente)
+    return {"mensaje": "Cliente eliminado correctamente"}

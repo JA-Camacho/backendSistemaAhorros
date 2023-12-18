391fd2c40f391367ddce63f5971249350bc7db40
Josue Cueva <jcuevag2@est.ups.edu.ec>
2023-12-18 08:37:53 -0500

    add get Transacciones

--- controllers/transaccion_controllers.py
+++ controllers/transaccion_controllers.py
@@ -1,5 +1,29 @@
 from models import Transaccion
+from sqlalchemy import text
+from fastapi import HTTPException
+from database.database import engine
 
 def create_transaccion(transaccion: Transaccion):
-    # Lógica para crear una transacción
-    return {"mensaje": "Transaccion exitosa", "transaccion": transaccion}
+    try:
+        with engine.connect() as connection:
+            query = text("INSERT INTO transacciones (id_cuenta, tipo_transaccion, monto, fecha_transaccion) "
+                         "VALUES (:id_cuenta, :tipo_transaccion, :monto, :fecha_transaccion)")
+            connection.execute(query, id_cuenta=transaccion.id_cuenta,
+                               tipo_transaccion=transaccion.tipo_transaccion,
+                               monto=transaccion.monto,
+                               fecha_transaccion=transaccion.fecha_transaccion)
+
+            return {"mensaje": "Transacción exitosa", "transaccion": transaccion.dict()}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al crear transacción: {str(e)}")
+
+def get_transacciones(id_cuenta: int):
+    try:
+        with engine.connect() as connection:
+            query = text("SELECT tipo_transaccion, monto FROM transacciones WHERE id_cuenta = :id_cuenta")
+            result = connection.execute(query, id_cuenta=id_cuenta).fetchall()
+
+            transacciones_data = [{"tipo_transaccion": row[0], "monto": row[1]} for row in result]
+            return transacciones_data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al obtener transacciones de la cuenta: {str(e)}")
--- views/transaccion_views.py
+++ views/transaccion_views.py
@@ -1,9 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException
-from controllers import create_cliente
 from models.transaccion import Transaccion
+from controllers.transaccion_controllers import get_transacciones, create_transaccion
 
 router = APIRouter()
 
 @router.post("/transacciones/")
-async def create_transaccion(transaccion: Transaccion):
-    return {"mensaje": "Transaccion exitosa", "transaccion": transaccion}
+async def create_transaccion_endpoint(transaccion: Transaccion):
+    result = create_transaccion(transaccion)
+    return {"mensaje": result["mensaje"], "transaccion": result["transaccion"]}
+
+@router.get("/transacciones/{id_cuenta}")
+async def get_transacciones_por_cuenta_endpoint(id_cuenta: int):
+    try:
+        transacciones = get_transacciones(id_cuenta)
+        return {"mensaje": "Transacciones obtenidas correctamente", "transacciones": transacciones}
+    except HTTPException as e:
+        return {"mensaje": "Error al procesar la solicitud", "detalle": e.detail}

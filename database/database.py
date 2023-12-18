966e5ba4a7973eecce283fd98c8b047d8fdf4964
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-12-17 21:07:23 -0500

    Fixing error in database connection and querys

--- database/database.py
+++ database/database.py
@@ -1,41 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table
-from databases import Database
-from sqlalchemy.orm import sessionmaker
-from contextlib import contextmanager
+from fastapi import FastAPI
+from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql+psycopg2://postgres:123@localhost/ahorros"
+DATABASE_URL = "postgresql://postgres:1234@localhost:5432/ahorros"
+
 engine = create_engine(DATABASE_URL)
-metadata = MetaData()
-
-database = Database(DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-clientes = Table(
-    "clientes",
-    metadata,
-    Column("id_cliente", Integer, primary_key=True, index=True),
-    Column("nombre", String, index=True),
-    Column("correo_electronico", String, index=True),
-    Column("password", String, index=True)
-)
-
-@contextmanager
-def get_session():
-    session = SessionLocal()
-    try:
-        yield session
-    finally:
-        session.close()
-
-async def execute(query, values=None):
-    async with database.transaction():
-        return await database.execute(query, values=values)
-
-async def fetch_one(query, values=None):
-    async with database.transaction():
-        return await database.fetch_one(query, values=values)
-
-async def fetch_all(query, values=None):
-    async with database.transaction():
-        return await database.fetch_all(query, values=values)
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from fastapi import FastAPI
 from views.cliente_views import router as cliente_router
-from database.database import database
+from database.database import engine
 
 app = FastAPI()
 
-async def open_database_connection_pools():
-    await database.connect()
+def open_database_connection_pools():
+    engine.connect()
 
-async def close_database_connection_pools():
-    await database.disconnect()
+def close_database_connection_pools():
+    engine.disconnect()
 
 app.add_event_handler("startup", open_database_connection_pools)
 app.add_event_handler("shutdown", close_database_connection_pools)
+app.include_router(cliente_router)
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-app.include_router(cliente_router)
+    return {"Hello":"World"}

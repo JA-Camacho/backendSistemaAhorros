06443047a163cb34d62b5deed5890245e27b315b
Josue Cueva <jcuevag2@est.ups.edu.ec>
2023-12-18 08:38:41 -0500

    modify main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from views.cliente_views import router as cliente_router
 from database.database import engine
 from views.cuenta_views import router as cuenta_router
+from views.transaccion_views import router as transaccion_router
 
 app = FastAPI()
 
@@ -11,6 +12,8 @@ def open_database_connection_pools():
 app.add_event_handler("startup", open_database_connection_pools)
 app.include_router(cliente_router)
 app.include_router(cuenta_router)
+app.include_router(transaccion_router)
+
 
 @app.get("/")
 def read_root():

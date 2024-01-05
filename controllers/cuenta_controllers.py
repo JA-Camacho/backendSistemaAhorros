575437b8797fddff805cd3d983c6b2c45ceda034
Gilmar Campoverde <gcampoverdea@est.ups.edu.ec>
2024-01-04 23:31:55 -0500

    Update cuentactrl

--- controllers/cuenta_controllers.py
+++ controllers/cuenta_controllers.py
@@ -1,21 +1,21 @@
-from models.cuenta_ahorro import Cuenta_Ahorro
+# controllers/cuenta_controllers.py
 from sqlalchemy import text
 from fastapi import HTTPException
-from controllers.cliente_controllers import valor_md5
 from database.database import engine
+from controllers.cliente_controllers import valor_md5
 
-def create_cuenta(cuenta: Cuenta_Ahorro):
+def create_cuenta(cuenta):
     with engine.connect() as connection:
         query = text("INSERT INTO cuentas_ahorro (id_cuenta, id_usuario, saldo) VALUES (:id_cuenta, :id_usuario, :saldo)")
         connection.execute(query, id_cuenta=cuenta.id_cuenta, id_usuario=cuenta.id_usuario, saldo=cuenta.saldo)
     return {"mensaje": "Cuenta creada exitosamente", "cuenta": cuenta}
 
-def delete_cuenta(cuenta: Cuenta_Ahorro):
+def delete_cuenta(cuenta):
     with engine.connect() as connection:
         query = text("DELETE FROM cuentas_ahorro WHERE id_cuenta = :id_cuenta")
         connection.execute(query, id_cuenta=cuenta.id_cuenta)
 
-def get_cuentas_usuario(correo: str, contrasena: str):
+def get_cuentas_usuario(correo, contrasena):
     try:
         with engine.connect() as connection:
             query = text("SELECT id_cuenta, id_usuario, saldo FROM cuentas_ahorro WHERE id_usuario = ("
@@ -24,6 +24,8 @@ def get_cuentas_usuario(correo: str, contrasena: str):
             result = connection.execute(query, correo_electronico=correo, contra_md5=valor_md5(contrasena)).fetchall()
 
             cuentas_data = [dict(row) for row in result]
-            return cuentas_data
+            return {"mensaje": "Cuentas obtenidas correctamente", "cuentas": cuentas_data}
+    except HTTPException:
+        raise
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error al obtener cuentas del usuario: {str(e)}")
